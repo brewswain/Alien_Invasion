43bf9141f0b1f163c1525c8eeae240866f403e00
brewswain <brandonlee2543@hotmail.com>
2019-06-10 15:53:54 -0400

    Refactor code further

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,3 @@
-import sys
 import pygame
 from settings import Settings
 from ship import Ship
@@ -18,15 +17,8 @@ def run_game():
 
     # Start the main loop for the game
     while True:
-
         gf.check_events()
-
-        # Redraw the screen during each pass through the loop.
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+        gr.update_screen(ai_settings, screen, ship)
 
 
 run_game()
