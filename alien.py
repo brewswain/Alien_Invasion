f96b9e60d264591078b5b089fc4ffd5f69e72f82
brewswain <brandonlee2543@hotmail.com>
2019-06-15 12:22:46 -0400

    Refine Alien Movement.

--- alien.py
+++ alien.py
@@ -13,7 +13,7 @@ class Alien(Sprite):
 
         # Load the alien image and set its rect attribute.
         self.image = pygame.image.load(
-            r'C:\Users\brand\OneDrive\Documents\Web Dev\Python\PCC\Python_Crash_Course_WIP\Part II - Projects\Project 1 - Alien Invasion\Project Files\images\alien.bmp')
+            r'C:\Users\brand\OneDrive\Documents\Web Dev\Python\Alien_Invasion\images\alien.bmp')
         self.rect = self.image.get_rect()
 
         # Start each new alien near the top left of the screen.
@@ -26,3 +26,17 @@ class Alien(Sprite):
     def blitme(self):
         """Draw the alien at its current location."""
         self.screen.blit(self.image, self.rect)
+
+    def check_edges(self):
+        """Return True if alien is at edge of screen."""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <= 0:
+            return True
+
+    def update(self):
+        """Move the alien right or left."""
+        self.x += (self.ai_settings.alien_speed_factor *
+                   self.ai_settings.fleet_direction)
+        self.rect.x = self.x
